2421530efd6c8c5c97708055cab76b5003e77088
JackTing <jackting@nine-yi.com>
2017-09-16 17:31:42 +0800

    加入第二個折扣條件，原測試有紅燈

--- TDD_HarryPotterShoppingCart/ShoppingCart.cs
+++ TDD_HarryPotterShoppingCart/ShoppingCart.cs
@@ -7,11 +7,27 @@ using TDD_HarryPotterShoppingCart.BE;
 
 namespace TDD_HarryPotterShoppingCart
 {
+    /// <summary>
+    /// ShoppingCart
+    /// </summary>
     public class ShoppingCart
     {
+        /// <summary>
+        /// Calculate
+        /// </summary>
+        /// <param name="shoppingCart">The shopping cart.</param>
+        /// <returns>總金額</returns>
         public decimal Calculate(ShoppingCartEntity shoppingCart)
         {
+            //// 1.一到五集的哈利波特，每一本都是賣100元
+            //// 2.如果你從這個系列買了兩本不同的書，則會有5%的折扣
+            var count = shoppingCart.SalePageList.Select(i => i.Id).Distinct().Count();
+
             var totalPrice = shoppingCart.SalePageList.Sum(i => i.Qty * i.Price);
+            if (count >= 2)
+            {
+                totalPrice = totalPrice * 0.95m;
+            }
 
             return totalPrice;
         }
