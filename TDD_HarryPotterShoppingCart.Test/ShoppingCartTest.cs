7bc1d43deb8486646adfcb36b92cb03deb85e070
JackTing <jackting@nine-yi.com>
2017-09-17 22:43:55 +0800

    測試重構，使用MemberData

--- TDD_HarryPotterShoppingCart.Test/ShoppingCartTest.cs
+++ TDD_HarryPotterShoppingCart.Test/ShoppingCartTest.cs
@@ -1,175 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TDD_HarryPotterShoppingCart.BE;
+﻿using TDD_HarryPotterShoppingCart.BE;
 using Xunit;
 
 namespace TDD_HarryPotterShoppingCart.Test
 {
     public class ShoppingCartTest
     {
-        [Fact]
-        public void Buy_1_HarryPort1_TotalPrice_Should_Be_100()
+        [Theory]
+        [MemberData(nameof(ShoppingCartTestData.TestData), MemberType = typeof(ShoppingCartTestData))]
+        public void ShoppingCartCalculateTest(ShoppingCartEntity shoppingCart, decimal expected)
         {
-            //// Arrange
-            var expected = 100;
+            // Arrange
+            var actual = 0m;
 
+            // Act
             var target = new ShoppingCart();
-
-            var shoppingCart = new ShoppingCartEntity
-            {
-                SalePageList = new List<SalePageEntity>()
-                {
-                    new SalePageEntity(){Id = 1, Price = 100, Qty = 1}
-                }
-            };
-
-            decimal actual = 0m;
-
-            //// Act
             actual = target.Calculate(shoppingCart);
 
-            //// Assert
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void Buy_HarryPort_Different2_TotalPrice_Should_Be_190()
-        {
-            //// Arrange
-            var expected = 190;
-
-            var target = new ShoppingCart();
-
-            var shoppingCart = new ShoppingCartEntity
-            {
-                SalePageList = new List<SalePageEntity>()
-                {
-                    new SalePageEntity(){Id = 1, Price = 100, Qty = 1},
-                    new SalePageEntity(){Id = 2, Price = 100, Qty = 1}
-                }
-            };
-
-            decimal actual = 0m;
-
-            //// Act
-            actual = target.Calculate(shoppingCart);
-
-            //// Assert
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void Buy_HarryPort_Different3_TotalPrice_Should_Be_270()
-        {
-            //// Arrange
-            var expected = 270;
-
-            var target = new ShoppingCart();
-
-            var shoppingCart = new ShoppingCartEntity
-            {
-                SalePageList = new List<SalePageEntity>()
-                {
-                    new SalePageEntity(){Id = 1, Price = 100, Qty = 1},
-                    new SalePageEntity(){Id = 2, Price = 100, Qty = 1},
-                    new SalePageEntity(){Id = 3, Price = 100, Qty = 1}
-                }
-            };
-
-            decimal actual = 0m;
-
-            //// Act
-            actual = target.Calculate(shoppingCart);
-
-            //// Assert
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void Buy_HarryPort_Different4_TotalPrice_Should_Be_320()
-        {
-            //// Arrange
-            var expected = 320;
-
-            var target = new ShoppingCart();
-
-            var shoppingCart = new ShoppingCartEntity
-            {
-                SalePageList = new List<SalePageEntity>()
-                {
-                    new SalePageEntity(){Id = 1, Price = 100, Qty = 1},
-                    new SalePageEntity(){Id = 2, Price = 100, Qty = 1},
-                    new SalePageEntity(){Id = 3, Price = 100, Qty = 1},
-                    new SalePageEntity(){Id = 4, Price = 100, Qty = 1}
-                }
-            };
-
-            decimal actual = 0m;
-
-            //// Act
-            actual = target.Calculate(shoppingCart);
-
-            //// Assert
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void Buy_HarryPort_Different5_TotalPrice_Should_Be_375()
-        {
-            //// Arrange
-            var expected = 375;
-
-            var target = new ShoppingCart();
-
-            var shoppingCart = new ShoppingCartEntity
-            {
-                SalePageList = new List<SalePageEntity>()
-                {
-                    new SalePageEntity(){Id = 1, Price = 100, Qty = 1},
-                    new SalePageEntity(){Id = 2, Price = 100, Qty = 1},
-                    new SalePageEntity(){Id = 3, Price = 100, Qty = 1},
-                    new SalePageEntity(){Id = 4, Price = 100, Qty = 1},
-                    new SalePageEntity(){Id = 5, Price = 100, Qty = 1}
-                }
-            };
-
-            decimal actual = 0m;
-
-            //// Act
-            actual = target.Calculate(shoppingCart);
-
-            //// Assert
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void Buy_HarryPort_Different3_Same1_TotalPrice_Should_Be_370()
-        {
-            //// Arrange
-            var expected = 370;
-
-            var target = new ShoppingCart();
-
-            var shoppingCart = new ShoppingCartEntity
-            {
-                SalePageList = new List<SalePageEntity>()
-                {
-                    new SalePageEntity(){Id = 1, Price = 100, Qty = 1},
-                    new SalePageEntity(){Id = 2, Price = 100, Qty = 1},
-                    new SalePageEntity(){Id = 3, Price = 100, Qty = 1},
-                    new SalePageEntity(){Id = 3, Price = 100, Qty = 1},
-                }
-            };
-
-            decimal actual = 0m;
-
-            //// Act
-            actual = target.Calculate(shoppingCart);
-
-            //// Assert
+            // Arrange
             Assert.Equal(expected, actual);
         }
     }
---
+++ TDD_HarryPotterShoppingCart.Test/ShoppingCartTestData.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDD_HarryPotterShoppingCart.BE;
+
+namespace TDD_HarryPotterShoppingCart.Test
+{
+    public class ShoppingCartTestData
+    {
+        public static IEnumerable<object[]> TestData
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[]
+                    {
+                        new ShoppingCartEntity
+                        {
+                            SalePageList = new List<SalePageEntity>()
+                            {
+                                new SalePageEntity(){Id = 1, Price = 100, Qty = 1}
+                            }
+                        },
+                        100M
+                    },
+                    new object[]
+                    {
+                        new ShoppingCartEntity
+                        {
+                            SalePageList = new List<SalePageEntity>()
+                            {
+                                new SalePageEntity(){Id = 1, Price = 100, Qty = 1},
+                                new SalePageEntity(){Id = 2, Price = 100, Qty = 1}
+                            }
+                        },
+                        190M
+                    },
+                    new object[]
+                    {
+                        new ShoppingCartEntity
+                        {
+                            SalePageList = new List<SalePageEntity>()
+                            {
+                                new SalePageEntity(){Id = 1, Price = 100, Qty = 1},
+                                new SalePageEntity(){Id = 2, Price = 100, Qty = 1},
+                                new SalePageEntity(){Id = 3, Price = 100, Qty = 1}
+                            }
+                        },
+                        270M
+                    },
+                    new object[]
+                    {
+                        new ShoppingCartEntity
+                        {
+                            SalePageList = new List<SalePageEntity>()
+                            {
+                                new SalePageEntity(){Id = 1, Price = 100, Qty = 1},
+                                new SalePageEntity(){Id = 2, Price = 100, Qty = 1},
+                                new SalePageEntity(){Id = 3, Price = 100, Qty = 1},
+                                new SalePageEntity(){Id = 4, Price = 100, Qty = 1}
+                            }
+                        },
+                        320M
+                    },
+                    new object[]
+                    {
+                        new ShoppingCartEntity
+                        {
+                            SalePageList = new List<SalePageEntity>()
+                            {
+                                new SalePageEntity(){Id = 1, Price = 100, Qty = 1},
+                                new SalePageEntity(){Id = 2, Price = 100, Qty = 1},
+                                new SalePageEntity(){Id = 3, Price = 100, Qty = 1},
+                                new SalePageEntity(){Id = 4, Price = 100, Qty = 1},
+                                new SalePageEntity(){Id = 5, Price = 100, Qty = 1}
+                            }
+                        },
+                        375M
+                    },
+                    new object[]
+                    {
+                        new ShoppingCartEntity
+                        {
+                            SalePageList = new List<SalePageEntity>()
+                            {
+                                new SalePageEntity(){Id = 1, Price = 100, Qty = 1},
+                                new SalePageEntity(){Id = 2, Price = 100, Qty = 1},
+                                new SalePageEntity(){Id = 3, Price = 100, Qty = 1},
+                                new SalePageEntity(){Id = 3, Price = 100, Qty = 1},
+                            }
+                        },
+                        370M
+                    }
+                };
+            }
+        }
+    }
+}
